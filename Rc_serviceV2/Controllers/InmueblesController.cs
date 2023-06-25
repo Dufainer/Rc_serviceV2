a32576ec5bfd2398db82e92846c02167749d036f
Cristian Pardo <cristianpardo044@gmail.com>
2023-06-25 01:52:25 -0500

    Crud Propietario e Imnuebles

---
+++ Rc_serviceV2/Controllers/InmueblesController.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Rc_serviceV2.Models;
+
+namespace Rc_serviceV2.Controllers
+{
+    public class InmueblesController : Controller
+    {
+        private readonly Rc_serviceContext _context;
+
+        public InmueblesController(Rc_serviceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Inmuebles
+        public async Task<IActionResult> Index(string searchId)
+        {
+            IQueryable<Inmueble> inmuebleQuery = _context.Inmuebles.Include(i => i.PropietariosIdPropietarioNavigation);
+
+
+            if (!string.IsNullOrEmpty(searchId))
+            {
+                inmuebleQuery = inmuebleQuery.Where(i => i.IdInmueble.ToString() == searchId);
+            }
+
+            var inmuebles = await inmuebleQuery.ToListAsync();
+
+            if (inmuebles.Count == 0)
+            {
+                ViewBag.ErrorMessage = "El inmueble no existe.";
+            }
+
+
+            return View(inmuebles);
+        }
+
+        //METODO PARA EL BUSCADOR AUTOCOMPLETE
+        public IActionResult GetPropietario(string term)
+        {
+            var result = (from U in _context.Propietarios.ToList()
+                          where U.IdPropietario.Contains(term, System.StringComparison.CurrentCultureIgnoreCase)
+                          select new { value = U.IdPropietario }
+                          );
+            return Json(result);
+        }
+
+        // GET: Inmuebles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Inmuebles == null)
+            {
+                return NotFound();
+            }
+
+            var inmueble = await _context.Inmuebles
+                .Include(i => i.PropietariosIdPropietarioNavigation)
+                .FirstOrDefaultAsync(m => m.IdInmueble == id);
+            if (inmueble == null)
+            {
+                return NotFound();
+            }
+
+            return View(inmueble);
+        }
+
+        // GET: Inmuebles/Create
+        public IActionResult Create()
+        {
+            ViewData["PropietariosIdPropietario"] = new SelectList(_context.Propietarios, "IdPropietario", "IdPropietario");
+            return View();
+        }
+
+        // POST: Inmuebles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdInmueble,TipoInmueble,DetallesInmueble,UbicacionId,PropietariosIdPropietario")] Inmueble inmueble)
+        {
+            if (ModelState.IsValid)
+            {
+                // Obtiene el valor seleccionado del campo de autocompletado
+                string propietarioId = Request.Form["PropietariosIdPropietario"];
+
+                // Asigna el valor seleccionado al modelo
+                inmueble.PropietariosIdPropietario = propietarioId;
+
+                _context.Add(inmueble);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["PropietariosIdPropietario"] = new SelectList(_context.Propietarios, "IdPropietario", "IdPropietario", inmueble.PropietariosIdPropietario);
+            return View(inmueble);
+        }
+
+        // GET: Inmuebles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Inmuebles == null)
+            {
+                return NotFound();
+            }
+
+            var inmueble = await _context.Inmuebles.FindAsync(id);
+            if (inmueble == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["PropietariosIdPropietario"] = new SelectList(_context.Propietarios, "IdPropietario", "IdPropietario", inmueble.PropietariosIdPropietario);
+
+            // Agrega el valor seleccionado al ViewBag para usarlo en la vista
+            ViewBag.PropietarioValue = inmueble.PropietariosIdPropietario;
+
+            return View(inmueble);
+        }
+
+        // POST: Inmuebles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdInmueble,TipoInmueble,DetallesInmueble,UbicacionId,PropietariosIdPropietario")] Inmueble inmueble)
+        {
+            if (id != inmueble.IdInmueble)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                // Obtiene el valor seleccionado del campo de autocompletado
+                string propietarioId = Request.Form["PropietariosIdPropietario"];
+
+                // Asigna el valor seleccionado al modelo
+                inmueble.PropietariosIdPropietario = propietarioId;
+
+                try
+                {
+                    _context.Update(inmueble);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InmuebleExists(inmueble.IdInmueble))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PropietariosIdPropietario"] = new SelectList(_context.Propietarios, "IdPropietario", "IdPropietario", inmueble.PropietariosIdPropietario);
+            return View(inmueble);
+        }
+
+
+        // GET: Inmuebles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Inmuebles == null)
+            {
+                return NotFound();
+            }
+
+            var inmueble = await _context.Inmuebles
+                .Include(i => i.PropietariosIdPropietarioNavigation)
+                .FirstOrDefaultAsync(m => m.IdInmueble == id);
+            if (inmueble == null)
+            {
+                return NotFound();
+            }
+
+            return View(inmueble);
+        }
+
+        // POST: Inmuebles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Inmuebles == null)
+            {
+                return Problem("Entity set 'Rc_serviceContext.Inmuebles'  is null.");
+            }
+            var inmueble = await _context.Inmuebles.FindAsync(id);
+            if (inmueble != null)
+            {
+                _context.Inmuebles.Remove(inmueble);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InmuebleExists(int id)
+        {
+            return (_context.Inmuebles?.Any(e => e.IdInmueble == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Rc_serviceV2/Controllers/PropietariosController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Rc_serviceV2.Models;
+
+namespace Rc_serviceV2.Controllers
+{
+    public class PropietariosController : Controller
+    {
+        private readonly Rc_serviceContext _context;
+
+        public PropietariosController(Rc_serviceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Propietarios
+        public async Task<IActionResult> Index(string searchId)
+        {
+            IQueryable<Propietario> propietarioQuery = _context.Propietarios;
+
+            if (!string.IsNullOrEmpty(searchId))
+            {
+                propietarioQuery = propietarioQuery.Where(p => p.IdPropietario.Contains(searchId));
+            }
+
+            var propietarios = await propietarioQuery.ToListAsync();
+
+            if (propietarios.Count == 0)
+            {
+                ViewBag.ErrorMessage = "El propietario no existe.";
+            }
+
+
+            return View(propietarios);
+        }
+
+        // GET: Propietarios/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Propietarios == null)
+            {
+                return NotFound();
+            }
+
+            var propietario = await _context.Propietarios
+                .FirstOrDefaultAsync(m => m.IdPropietario == id);
+            if (propietario == null)
+            {
+                return NotFound();
+            }
+
+            return View(propietario);
+        }
+
+        // GET: Propietarios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Propietarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPropietario,NamePropietario,LastnamePropietario,Celular,Email,UbicacionId")] Propietario propietario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(propietario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(propietario);
+        }
+
+        // GET: Propietarios/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Propietarios == null)
+            {
+                return NotFound();
+            }
+
+            var propietario = await _context.Propietarios.FindAsync(id);
+            if (propietario == null)
+            {
+                return NotFound();
+            }
+            return View(propietario);
+        }
+
+        // POST: Propietarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("IdPropietario,NamePropietario,LastnamePropietario,Celular,Email,UbicacionId")] Propietario propietario)
+        {
+            if (id != propietario.IdPropietario)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(propietario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PropietarioExists(propietario.IdPropietario))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(propietario);
+        }
+
+        // GET: Propietarios/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Propietarios == null)
+            {
+                return NotFound();
+            }
+
+            var propietario = await _context.Propietarios
+                .FirstOrDefaultAsync(m => m.IdPropietario == id);
+            if (propietario == null)
+            {
+                return NotFound();
+            }
+
+            return View(propietario);
+        }
+
+        // POST: Propietarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Propietarios == null)
+            {
+                return Problem("Entity set 'Rc_serviceContext.Propietarios'  is null.");
+            }
+            var propietario = await _context.Propietarios.FindAsync(id);
+            if (propietario != null)
+            {
+                _context.Propietarios.Remove(propietario);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PropietarioExists(string id)
+        {
+            return (_context.Propietarios?.Any(e => e.IdPropietario == id)).GetValueOrDefault();
+        }
+    }
+}
